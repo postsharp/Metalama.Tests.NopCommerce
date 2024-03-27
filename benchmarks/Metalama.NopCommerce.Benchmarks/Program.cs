f03ab825348e02f493ec79301bb210ef9b202a98
Petr Onderka <petro@postsharp.net>
2024-03-27 12:02:26 +0100

    #34363 Added benchmark

---
+++ benchmarks/Metalama.NopCommerce.Benchmarks/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchmark>();
+
+public class Benchmark
+{
+    static async Task RunProcess(string fileName, IEnumerable<string> arguments)
+    {
+        var startInfo = new ProcessStartInfo(fileName)
+        {
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true
+        };
+
+        foreach (var argument in arguments)
+        {
+            startInfo.ArgumentList.Add(argument);
+        }
+
+        using var process = new Process { StartInfo = startInfo };
+
+        var sb = new StringBuilder();
+
+        process.OutputDataReceived += appendLine;
+        process.ErrorDataReceived += appendLine;
+
+        void appendLine(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                lock (sb)
+                {
+                    sb.AppendLine(e.Data);
+                }
+            }
+        }
+
+        process.Start();
+
+        process.BeginErrorReadLine();
+        process.BeginOutputReadLine();
+
+        await process.WaitForExitAsync();
+
+        if (process.ExitCode != 0)
+        {
+            throw new Exception($"""
+                Build failed with code {process.ExitCode}:
+                {sb}
+                """);
+        }
+    }
+
+    private static Task RunDotnetBuild(string project, Dictionary<string, string> properties)
+        => RunProcess("dotnet", ["build", Path.Combine(_repoRoot, project), .. properties.Select(p => $"-p:{p.Key}={p.Value}")]);
+
+    private static Task RunDotnetClean(string project)
+        => RunProcess("dotnet", ["clean", Path.Combine(_repoRoot, project)]);
+
+    private static string FindRepoRoot()
+    {
+        var dir = Environment.CurrentDirectory;
+
+        while (dir != null)
+        {
+            if (Directory.Exists(Path.Combine(dir, ".git")))
+            {
+                return dir;
+            }
+
+            dir = Path.GetDirectoryName(dir);
+        }
+
+        throw new InvalidOperationException($"Could not find repo root when starting from '{Environment.CurrentDirectory}'.");
+    }
+
+    private static readonly string _repoRoot = FindRepoRoot();
+
+    private const string SOLUTION = @"src\NopCommerce.sln";
+
+    [IterationSetup(Target = nameof(WithoutMetalama))]
+    public void SetupWithoutMetalama() => RunDotnetClean(SOLUTION).Wait();
+
+    [Benchmark(Baseline = true)]
+    public Task WithoutMetalama() => RunDotnetBuild(SOLUTION, new Dictionary<string, string>
+    {
+        ["MetalamaEnabled"] = "false",
+        ["ExtraConstants"] = "BENCHMARK"
+    });
+
+    [IterationSetup(Target = nameof(WithMetalama))]
+    public void SetupWithMetalama() => RunDotnetClean(SOLUTION).Wait();
+
+    [Benchmark]
+    public Task WithMetalama() => RunDotnetBuild(SOLUTION, new Dictionary<string, string>
+    {
+        ["MetalamaEnabled"] = "true",
+        ["ExtraConstants"] = "BENCHMARK"
+    });
+}
---
+++ src/Metalama.Aspects/Metalama.Aspects/BenchmarkAspects.cs
@@ -0,0 +1,95 @@
+﻿#if BENCHMARK
+
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using Metalama.Framework.Code.SyntaxBuilders;
+using Metalama.Framework.Fabrics;
+
+namespace Metalama.Aspects;
+
+internal class NotNullFabric : TransitiveProjectFabric
+{
+    public override void AmendProject(IProjectAmender amender)
+    {
+        static IEnumerable<IMethod> getMethods(IMember member) => member switch
+        {
+            IMethod method => [method],
+            IHasAccessors hasAccessors => hasAccessors.Accessors,
+            IConstructor => [],
+            _ => throw new NotSupportedException($"Unexpected member '{member}'")
+        };
+
+        amender.Outbound
+            .SelectMany(p => p.AllTypes)
+            .SelectMany(t => t.Members())
+            .SelectMany(getMethods)
+            .Where(m => GetStringHashCode(m.ToDisplayString(CodeDisplayFormat.FullyQualified)) % 10 == 0)
+            .AddAspectIfEligible<LogAspect>();
+    }
+
+    // From https://stackoverflow.com/a/36846609/41071.
+    private static int GetStringHashCode(string s)
+    {
+        unchecked
+        {
+            int hash1 = 5381;
+            int hash2 = hash1;
+
+            for (int i = 0; i < s.Length && s[i] != '\0'; i += 2)
+            {
+                hash1 = ((hash1 << 5) + hash1) ^ s[i];
+                if (i == s.Length - 1 || s[i + 1] == '\0')
+                    break;
+                hash2 = ((hash2 << 5) + hash2) ^ s[i + 1];
+            }
+
+            return hash1 + (hash2 * 1566083941);
+        }
+    }
+}
+
+internal class LogAspect : OverrideMethodAspect
+{
+    public override dynamic? OverrideMethod()
+    {
+        var entryMessage = BuildInterpolatedString();
+        entryMessage.AddText(" called.");
+        Console.WriteLine(entryMessage.ToValue());
+
+        return meta.Proceed();
+    }
+
+    private static InterpolatedStringBuilder BuildInterpolatedString()
+    {
+        var stringBuilder = new InterpolatedStringBuilder();
+
+        stringBuilder.AddText(meta.Target.Type.ToDisplayString(CodeDisplayFormat.MinimallyQualified));
+        stringBuilder.AddText(".");
+        stringBuilder.AddText(meta.Target.Method.Name);
+        stringBuilder.AddText("(");
+        var first = meta.CompileTime(true);
+
+        foreach (var p in meta.Target.Parameters)
+        {
+            var comma = first ? "" : ", ";
+
+            if (p.RefKind == RefKind.Out)
+            {
+                stringBuilder.AddText($"{comma}out {p.Name}");
+            }
+            else
+            {
+                stringBuilder.AddText($"{comma}{p.Name}: ");
+                stringBuilder.AddExpression(p.Value);
+            }
+
+            first = false;
+        }
+
+        stringBuilder.AddText(")");
+
+        return stringBuilder;
+    }
+}
+
+#endif
